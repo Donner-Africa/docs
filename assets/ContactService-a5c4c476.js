d3fbc20485fda7704033b99c2f4532d313efe988
Chibueze Harry <buezeharry@gmail.com>
2026-02-17 18:16:35 +0100

    update: build

--- assets/ContactService-a5c4c476.js
+++
@@ -1,1 +0,0 @@
-import{ai as t}from"./index-1124f981.js";const o={async postMessage(s){return await t.post("contact",{authorizeRequest:!1,data:{...s}})}};export{o as C};
--- assets/NavigationStore-ce64ee39.js
+++
@@ -1,1 +0,0 @@
-import{ah as t}from"./index-1124f981.js";const a=t("navigation",{state:()=>({showNavigationBar:!0,showFooter:!0}),getters:{},actions:{}});export{a as g};
