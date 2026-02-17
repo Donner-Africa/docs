f4ce030a5f33e5f00eb32f26fa72a35f9b3d1b0f
Chibueze Harry <buezeharry@gmail.com>
2026-02-17 18:34:01 +0100

    update: build

--- assets/ComplianceService-a07dc706.js
+++
@@ -1,1 +0,0 @@
-import{ai as s}from"./index-c49e93e4.js";const n={async fetchUserComplianceData(e){return await s.get(`/me/compliances/${e}`,{authorizeRequest:!0})},async saveComplianceData(e,a,t){return await s.patch(`/me/compliances/${e}/${t}`,{authorizeRequest:!0,data:{complianceData:a}})}};export{n as C};
--- assets/NavigationStore-4749d2b5.js
+++
@@ -1,1 +0,0 @@
-import{ah as t}from"./index-c49e93e4.js";const a=t("navigation",{state:()=>({showNavigationBar:!0,showFooter:!0}),getters:{},actions:{}});export{a as g};
